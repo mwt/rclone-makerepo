6d8927dc1e529a449b9526e99d3f3ef15baeb347
Matthew W. Thomas <matthew@mwt.me>
2022-05-07 15:51:45 -0500

    working build

--- .github/scripts/2-make-appdir.zsh
+++ .github/scripts/2-make-appdir.zsh
@@ -7,9 +7,6 @@ REPO_NAME="${REPO_DIR##*/}"
 # Clean folder
 "${REPO_DIR}/clean.zsh"
 
-# Setup folders
-"${REPO_DIR}/initial-setup/2-enviroment.zsh"
-
 mkdir "${REPO_DIR}/temp/"
 
 wget -O "${REPO_DIR}/temp/linuxdeploy-x86_64.AppImage" "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage"
--- initial-setup/1-install-packages.sh
+++ initial-setup/1-install-packages.sh
@@ -5,25 +5,7 @@
 #  - gpg: sign repos
 #  - wget: download json and packages
 #  - zsh: build script is in zsh
-#  - jq: process json files
-#  - git: to install reprepro
 #
-## APT repos:
-#  - reprepro: makes apt repos (fork installed after)
-#
-## YUM repos:
-#  - rpm: get version info for renaming
-#  - createrepo-c: make repo metadata
-#
-
-mkdir -p ./temp/
-
-# Download reprepro multiple versions
-wget -O ./temp/reprepro.deb https://mattwthomas.com/gh/bin/reprepro-multiple-versions_5.3.0-1.4_amd64.deb
 
 sudo apt update && sudo apt -y install \
-sed gpg wget zsh jq git \
-rpm createrepo-c \
-./temp/reprepro.deb
-
-rm -r ./temp
+sed gpg wget zsh git \
