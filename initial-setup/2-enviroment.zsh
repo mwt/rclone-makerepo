bb0208f365c7d4f42acf73e9a0ba300407237510
Matthew Thomas <matthew@mwt.me>
2022-05-07 19:23:30 -0500

    replace gpg key if there is an argument

--- initial-setup/2-enviroment.zsh
+++ initial-setup/2-enviroment.zsh
@@ -4,10 +4,15 @@ SCRIPT_DIR=${0:a:h}
 REPO_DIR=${SCRIPT_DIR:h}
 
 # cd to main repo dir
-cd "${SCRIPT_DIR}/.."
+cd "$SCRIPT_DIR/.."
 
 # create downloads folder
 mkdir -p ./staging
+
+# replace gpg code if there is an argument
+if [[ -z $1 ]] {
+    sed -i "s/B7BE5AC2/$1/" "$REPO_DIR/reprepro/conf/distributions"
+}
 
 # create reprepro options file
 cat << EOF > "$REPO_DIR/reprepro/conf/options"
